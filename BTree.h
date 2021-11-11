bf3d9dfc36199fd45b1338c9405f40998b7bde1d
Ling_yunchi <1005175850@qq.com>
2021-11-11 20:48:22 +0800

    qwq

--- BTree.h
+++ BTree.h
@@ -1,56 +1,24 @@
-﻿#pragma once
+#pragma once
+#include <vector>
+
+#include "Data.h"
+using namespace std;
+
+const int M = 3;
 
 class BTreeNode
 {
-	int* keys;	//关键字数组,对应下标孩子节点数组指向的BTreeNode中的所有关键字小于该关键字
-	int t;
-	int num;
-	bool leaf;
-	BTreeNode* parent;
-	BTreeNode** child;
-
+	int size_;
+	vector<int> keys_;
+	vector<BTreeNode*> children_;
+	vector<Data*> data_;
 public:
-	BTreeNode(BTreeNode* parent,int t, bool leaf);
-
-	BTreeNode* search(int key);
-
-	int find_key(int key);
-
-	void insert_non_full(int key);
-
-	void split_child(int idx, BTreeNode* y);
-
-	void remove(int key);
-
-	void remove_from_leaf(int idx);
-
-	void remove_from_non_leaf(int idx);
-
-	int get_pred(int idx);
-
-	int get_succ(int idx);
-
-	void fill(int idx);
-
-	void borrow_from_prev(int idx);
-	
-	void borrow_from_next(int idx);
-
-	void merge(int idx);
-	
-	friend class BTree;
+	BTreeNode();
 };
 
 class BTree
 {
-	BTreeNode* root;
-	int t;
-public:
-	BTree(int t) :root(nullptr), t(t){}
+
 	
-	BTreeNode* search(int k);
-
-	void insert(int key);
-
-	void remove(int key);
 };
+
